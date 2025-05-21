23c34ae7320a40ce95ff21bd71e2628fbcb2e6e2
Chuwenni <christiandaledechavez4@gmail.com>
2025-05-21 08:37:25 +0800

    Add files via upload

--- game.js
+++ game.js
@@ -1,78 +1,147 @@
-const answer = document.getElementById('answer')
-const result = document.getElementById('result')
-const submitBTn = document.getElementById('submitBtn')
-const kiss = document.getElementById('kissCount')
+const display = document.getElementById('display');
+const kissCount = document.getElementById('kiss-count');
+const result = document.getElementById('result');
+const hidden = document.getElementById('hidden');
+const btn = document.querySelector('btns')
 
+let kiss = 0;
 
-let kis = 0;
+function displaynum(input){
+  display.value += input;
+}
 
-submitBTn.onclick = function(){
-  let answers = answer.value;
-  if (answers == ''){
-    kis++;
-    result.textContent = 'wrong'
-    kiss.textContent = (`${kis} `)
-    return(kis)
+function Del(){
+  display.value = ''
+}
+
+function Enter(input){
+
+  if (display.value == '') {
+    result.textContent = 'Pls put a number'
+    result.style.color = 'white'
+    result.style.left = '33%'
+    kiss++;
+    kissCount.textContent = `${kiss}`
   }
-  else if(answers ==  1){
-    kis++;
-    result.textContent = 'wrong';
-    kiss.textContent = (`${kis}`)
+  else if(display.value == 1){
+    display.value = ''
+    result.textContent = 'Wrong'
+    result.style.color = 'red'
+    result.style.left = '45%'
+    kiss++;
+    kissCount.textContent = `${kiss}`
   }
-  else if(answers ==  2){
-    kis++;
-    result.textContent = 'wrong';
-    kiss.textContent = (`${kis}`)
+  else if(display.value == 2){
+    display.value = ''
+    result.textContent = 'Wrong'
+    result.style.color = 'red'
+    result.style.left = '45%'
+    kiss++;
+    kissCount.textContent = `${kiss}`
   }
-  else if(answers ==  3){
-    kis++;
-    result.textContent = 'wrong';
-    kiss.textContent = (`${kis}`)
+  else if(display.value == 3){
+    display.value = ''
+    result.textContent = 'Wrong'
+    result.style.color = 'red'
+    result.style.left = '45%'
+    kiss++;
+    kissCount.textContent = `${kiss}`
   }
-  else if(answers ==  4){
-    kis++;
-    result.textContent = 'wrong';
-    kiss.textContent = (`${kis}`)
+  else if(display.value == 4){
+    display.value = ''
+    result.textContent = 'Wrong'
+    result.style.color = 'red'
+    result.style.left = '45%'
+    kiss++;
+    kissCount.textContent = `${kiss}`
   }
-  else if(answers ==  5){
-    kis++;
-    result.textContent = 'wrong';
-    kiss.textContent = (`${kis}`)
+  else if(display.value == 5){
+    display.value = ''
+    result.textContent = 'Wrong'
+    result.style.color = 'red'
+    result.style.left = '45%'
+    kiss++;
+    kissCount.textContent = `${kiss}`
   }
-  else if(answers ==  6){
-    kis++;
-    result.textContent = 'wrong';
-    kiss.textContent = (`${kis}`)
+  else if(display.value == 6){
+    display.value = ''
+    result.textContent = 'Wrong'
+    result.style.color = 'red'
+    result.style.left = '45%'
+    kiss++;
+    kissCount.textContent = `${kiss}`
   }
-  else if(answers ==  7){
-    kis++;
-    result.textContent = 'wrong';
-    kiss.textContent = (`${kis}`)
+  else if(display.value == 7){
+    display.value = ''
+    result.textContent = 'Wrong'
+    result.style.color = 'red'
+    result.style.left = '45%'
+    kiss++;
+    kissCount.textContent = `${kiss}`
   }
-  else if(answers ==  8){
-    kis++;
-    result.textContent = 'wrong';
-    kiss.textContent = (`${kis}`)
+  else if(display.value == 8){
+    display.value = ''
+    result.textContent = 'Wrong'
+    result.style.color = 'red'
+    result.style.left = '45%'
+    kiss++;
+    kissCount.textContent = `${kiss}`
   }
-  else if(answers ==  9){
-    kis++;
-    result.textContent = 'wrong';
-    kiss.textContent = (`${kis}`)
+  else if(display.value == 9){
+    display.value = ''
+    result.textContent = 'Wrong'
+    result.style.color = 'red'
+    result.style.left = '45%'
+    kiss++;
+    kissCount.textContent = `${kiss}`
   }
-  else if(answers ==  10){
-    kis++;
-    result.textContent = 'wrong';
-    kiss.textContent = (`${kis}`)
+  else if(display.value == 10){
+    display.value = ''
+    result.textContent = 'Wrong'
+    result.style.color = 'red'
+    result.style.left = '45%'
+    kiss++;
+    kissCount.textContent = `${kiss}`
   }
-  else if(answers ==  0){
-    kis--;
-    result.textContent = 'correct';
-    kiss.textContent = (`${kis}`)
+  else if(display.value == 0){
+    display.value = ''
+    result.textContent = 'Correct'
+    result.style.color = 'rgb(0, 255, 0)'
+    result.style.left = '40%'
+    kiss--;
+    kissCount.textContent = `${kiss}`
   }
   else{
-    kis++;
-    result.textContent = 'put a real number';
-    kiss.textContent = (`${kis}`)
+    result.textContent = 'Pls Put 2 Digits Only';
+    result.style.left = '30%';
+    result.style.color = 'white'
   }
-  
-} 
+  if (kiss == 10){
+    hidden.textContent = 'ang dami pero kulang pa😋😋'
+    hidden.style.fontSize = '20px'
+  }
+  else if (kiss == 20) {
+    hidden.textContent = 'Oyy ang dami na busog nako'
+    hidden.style.fontSize = '20px'
+  }
+  else if (kiss == 25) {
+    hidden.textContent = 'Ay dinamihan talaga ah'
+    hidden.style.fontSize = '20px'
+  }
+  else if(kiss == 30) {
+    hidden.textContent = 'Pag dmoko kiniss ng ganto kadami dmoko love:<<'
+    hidden.style.fontSize = '20px'
+  }
+  else if (kiss == 35){
+    hidden.textContent = 'kikiss din kita ng ganto kadami:>>'
+    hidden.style.fontSize = '20px'
+  }
+  else if (kiss == 40){
+    hidden.textContent = 'dto na ung limit baka puro laway nko nan'
+    hidden.style.fontSize = '20px'
+  }
+  else if(kiss > 40){
+    kiss = 0
+    kissCount.textContent = `${kiss}`
+  }
+}
